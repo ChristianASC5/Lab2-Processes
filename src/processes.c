f29775bd8fcb96204f9e0636a5e67bd05767aaf8
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2023-09-30 19:54:55 -0400

    Reorganized files.

---
+++ src/processes.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+#include  <string.h>
+#include  <sys/types.h>
+
+#define   MAX_COUNT  200
+#define   BUF_SIZE   100
+
+void  main(void)
+{
+     pid_t  pid;
+     pid = fork();
+     
+     
+}
