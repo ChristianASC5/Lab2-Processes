77a2ff208cf42e42d99b1828ccd7d2243f90a6af
ChristianASC5 <cnicolas3411@gmail.com>
2023-10-04 14:04:39 -0400

    signals.c and timer.c implemenation

--- src/timer.c
+++ src/timer.c
@@ -1,10 +1,11 @@
-/* signal.c */
+/* timer.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 
 volatile int flag = 0;
+volatile int alarm_calls = 0;
 
 void handler(int signum)
 { // signal handler
@@ -12,9 +13,16 @@ void handler(int signum)
     flag = 1;
 }
 
+void time_handler(int signum)
+{
+    printf("\n%d seconds have passed\n", alarm_calls);
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
     signal(SIGALRM, handler); // register handler to handle SIGALRM
+    signal(SIGINT, time_handler);
 
     while (1)
     {
@@ -23,6 +31,8 @@ int main(int argc, char *argv[])
             ;
         printf("Turing was right\n");
         flag = 0;
+
+        alarm_calls++;
     }
 
     return 0; // never reached
